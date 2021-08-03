740e56ae30a291912d05014321d73b1937b8bcf5
Amandeep singh <tarlok6667@gmail.com>
2021-08-03 09:39:16 +0530

    Todo app commit 2

--- index.js
+++ index.js
@@ -17,26 +17,83 @@ var closelist = function(){
 
 
 var addvalue = ()=>{
-    var listvalue = document.getElementById("inputbox").value;
+  var inputBox = document.getElementById("inputbox").value;
+  var uid = Date.now();
+   count++;
+   if(inputBox != ""){
      const create_new_element = document.createElement("div");
-     create_new_element.innerHTML =` <div class="todo-list-container">
-     <p class="inner-text-1 id="">listvalue</p>
+     create_new_element.innerHTML =` <div class="todo-list-container" id="${Date.now()}">
+     <p class="inner-text-1" id="Innertext">${inputBox}</p>
      <div class="inner-container">
      <hr class="horizontal-line">
      <div>
      <span> <i class="fa fa-plus-circle addlist" id="add-item-icon style="font-size:36px"></i></span>
-     <span><i class="fas fa-trash-alt delicon" id="del-icon style=font-size:36px"></i></span>
+     <span><i class="fas fa-trash-alt delicon" onclick="deleteitem(this)" id="del-icon" style=font-size:"36px"></i></span>
     
      </div>
      </div> `;
-   let uid = Date.now();
      
-     var adddiv = document.getElementById("div-add");
-      adddiv.appendChild(create_new_element);
+     var adddiv = document.querySelector("#div-add")
+     adddiv.appendChild(create_new_element);
+     checknoitemdiv();
+   }
+   else{
+     addvalue();
+   }
+
+    
+     console.log(listarr)
      poplist.style.display = "none";
      container.classList.remove("blur");
-     document.querySelector(".inner-text-1").setAttribute("id",uid) ; 
-     listarr.pop(uid);
-    var para = document.getElementById(uid);
-    para.innerHTML = listvalue;
+     
+     }
+  
+   
+     
+
+// addHeading = ()=>{
+ 
+//   var uid = Date.now()
+  
+//   var mainHeading = {
+//     inputBox ,
+//     id : Date.now()
+//   }
+//   listarr.push(mainHeading);
+ 
+//   document.querySelector(".inner-text-1").setAttribute("id",uid);
+//   for(let i=0;i<listarr.length;i++){
+ 
+//       document.getElementById(mainHeading.id).innerText = inputBox;
+//   }
+//   console.log(listarr)
+  
+// }
+var noItem = document.getElementById("div-container")
+checknoitemdiv = ()=>{
+  if(count == 0){
+    noItem.style.display = "block";
+  }
+  else{
+    noItem.style.display = "none";
+  }
 }
+
+function deleteitem(currentelement){
+//  var currentelement = document.getElementById("del-icon");
+var currentdiv = currentelement.parentNode.parentElement.parentElement.parentElement;
+
+ var currentid = currentdiv.getAttribute("id")
+ var currentTodo = document.getElementById(currentid);
+ currentTodo.remove()
+ count--;
+
+    checknoitemdiv();
+  
+}
+  // for(let i =0;i<listarr.length;i++){
+  //   if(uid = listarr[i].id){
+  //     var node = document.getElementById(uid);
+  //     document.removeChild(node)
+  //   }
+  // }
